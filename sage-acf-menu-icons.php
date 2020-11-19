dc7d1423be728a252c969ac34936c64b12df0ee7
MWDelaney <michael.delaney@gmail.com>
2020-11-19 09:52:12 -0500

    Initial commit

---
+++ sage-acf-menu-icons.php
@@ -0,0 +1,59 @@
+<?php
+
+// Check whether WordPress and ACF are available; bail if not.
+if (! function_exists('get_field')) {
+    return;
+}
+if (! function_exists('add_filter')) {
+    return;
+}
+if (! function_exists('add_action')) {
+    return;
+}
+
+/**
+ * Add a JSON load point to ACF to add our fields
+ */
+add_filter('acf/settings/load_json', function ($paths) {
+
+    // append path
+    $paths[] = dirname(__FILE__). 'src/json';
+    print_r($paths);
+
+    // return
+    return $paths;
+});
+
+/**
+ * Add ACF menu icon to menus if present
+ */
+add_filter('wp_nav_menu_objects', function ($items, $args) {
+    // Loop through menu items
+    foreach ($items as &$item) {
+        // Get data from ACF fields
+        $icon = get_field('icon', $item);
+        $hide_label = get_field('hide_label', $item);
+        $position = get_field('position', $item);
+
+        // Get the icon contents
+        $icon_contents = file_get_contents($icon);
+
+        // Maybe hide label
+        if ($hide_label) {
+            // Wrap the title in a span with Bootstrap's screen-reader-only class
+            $item->title = '<span class="sr-only">' . $item->title . '</span>';
+        }
+
+        // If the icon exists, append icon
+        if ($icon) {
+            if ($position == 'before') {
+                $item->title = $icon_contents . $item->title;
+            } else {
+                $item->title = $item->title . $icon_contents;
+            }
+        }
+    }
+
+    // Return the items with icons
+    return $items;
+}, 10, 2);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5cd20984e319da41162651dd30f20a05a5f11481',
+    'name' => 'mwdelaney/sage-acf-menu-icons',
+  ),
+  'versions' => 
+  array (
+    'mwdelaney/sage-acf-menu-icons' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5cd20984e319da41162651dd30f20a05a5f11481',
+    ),
+  ),
+);
